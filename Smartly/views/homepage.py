7bbde43d48798dcfe6c60ed08171ab8981647f95
Jyothsna010106 <jyothsnajogi886@gmail.com>
2025-02-09 00:28:17 +0530

    24th so far created 2 html and course fetching on progress

--- Smartly/views/homepage.py
+++ Smartly/views/homepage.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from Smartly.models import Course
 from django.shortcuts import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Home Page..........")
+    Smartly = Course.objects.all()
+    print(Smartly)
+
+
+
+    return render(request,template_name="Smartly/home.html",
+                  context={"Smartly":Smartly} ) 
