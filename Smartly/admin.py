79324bbda502858d454624ac26a7629d209f0caa
Jyothsna010106 <jyothsnajogi886@gmail.com>
2025-02-08 18:40:21 +0530

    16th video and video section added

--- Smartly/admin.py
+++ Smartly/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from Smartly.models import Course , Tag , Prerequisite , Learning
+from Smartly.models import Course , Tag , Prerequisite , Learning,Video
 
 # Register your models here.
 
 class TagAdmin(admin.TabularInline):
     model = Tag
-
+    
+class VideoAdmin(admin.TabularInline):
+    model = Video
 class LearningAdmin(admin.TabularInline):
     model = Learning
 
@@ -14,6 +16,7 @@ class PrerequisiteAdmin(admin.TabularInline):
 
 
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [TagAdmin , LearningAdmin ,PrerequisiteAdmin]
+    inlines = [TagAdmin , LearningAdmin ,PrerequisiteAdmin,VideoAdmin]
 
 admin.site.register(Course,CourseAdmin)
+admin.site.register(Video)
--- Smartly/models/__init__.py
+++ Smartly/models/__init__.py
@@ -1,1 +1,2 @@
 from Smartly.models.course import Course , Learning , Prerequisite,Tag
+from Smartly.models.video import Video
