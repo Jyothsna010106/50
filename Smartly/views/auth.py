6c148e47ee08e998d5bc9d4f8ead3ec3f469afd2
Jyothsna010106 <jyothsnajogi886@gmail.com>
2025-02-12 23:24:04 +0530

    43 check

--- Smartly/views/auth.py
+++ Smartly/views/auth.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from Smartly.models import Course , Video
 from django.shortcuts import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request): 
-    return render(request,
-    template_name="Smartly/signup.html")
+    if(request.method =="GET"):
+      form = UserCreationForm()
+      return render(request,
+      template_name="Smartly/signup.html ", context= { 'form' : form })
+    
+    return HttpResponse("POST REQUEST")
 
 def login(request): 
     return render(request,
